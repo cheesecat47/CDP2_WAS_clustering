9921b76212159abac2611f177614679e1cdd6062
cheesecat47 <cheesecat47@gmail.com>
2020-04-12 21:13:09 +0900

    [25] Change jvmRoute name in server.xml
    Change jvmRoute name from balancer to node1 if the hostname is
    tomcat-server1 and node2 if tomcat-server2.
    After start the container, it starts the shell script to change the name
    in server.xml file.
    But yet it doesn't run well.

---
+++ tomcat_src/change_jvmRoute.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' = # Print a line
+echo "start change_jvmRoute.sh"
+
+tomcat1="tomcat-server1"
+tomcat2="tomcat-server2"
+
+hostname=$HOSTNAME
+
+echo "hostname: ${hostname}"
+echo "current line in server.xml"
+sed -n 133p < conf/server.xml
+
+if [ ${hostname} == ${tomcat1} ]; then
+    sed -i "133s/balancer/node1/" conf/server.xml
+    if [ $? -ne 0 ]
+    then
+        echo "Failed!"
+        exit 1
+    fi
+elif [ ${hostname} == ${tomcat2} ]; then
+    sed -i "133s/balancer/node2/" conf/server.xml
+    if [ $? -ne 0 ]
+    then
+        echo "Failed!"
+        exit 1
+    fi
+fi
+
+echo "after change"
+sed -n 133p < conf/server.xml
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' '#' # Print a line
