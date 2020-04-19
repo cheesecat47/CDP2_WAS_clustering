e964de38bae96dae6c445dd9e661777f3322de42
cheesecat47 <cheesecat47@gmail.com>
2020-04-19 11:05:13 +0900

    shell update1

--- start.sh
+++ start.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
 
-if [ ! -f /usr/local/bin/docker ]; then
-    echo "docker not exists, install docker first plz"
+echo "This script starts three containers:"
+echo "  - 1 apache httpd server"
+echo "  - 2 tomcat servers\n"
+
+THIS_OS="`uname -s`"
+echo "You are running on ${THIS_OS} machine."
+
+if [ ${THIS_OS} = "Linux" ]; then
+    # Request root privilege
+    [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+fi
+
+# Check Installed Components
+IS_DOCKER="`docker -v`"
+IS_DKCP="`docker-compose -v`"
+
+if [ ${IS_DOCKER:0:6} == "Docker" ]; then
+    echo "Docker ----------- ok"
+else
+    echo "docker is not installed, install docker first plz"
     exit 1
 fi
+
+if [ ${IS_DKCP:0:15} == "docker-compose" ]; then
+    echo "Docker-compose --- ok"
+else
+    echo "docker-compose is not installed, install docker-compose first plz"
+    exit 1
+fi
+
 
 docker-compose up -d --build 
--- terminate.sh
+++ terminate.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+echo "This script stops the running containers"
+echo "and remove related images.\n"
+
+THIS_OS="`uname -s`"
+echo "You are running on ${THIS_OS} machine."
+
+if [ ${THIS_OS} = "Linux" ]; then
+    # Request root privilege
+    [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+fi
+
 docker-compose down
 docker rmi myapache
 docker rmi mytomcat
